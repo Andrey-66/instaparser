9266f650b8a8993ae63fd2678ee6e4975ae93384
Andrey66 <andrey66-66-66@yandex.ru>
2025-10-21 14:53:14 +0300

    Fix anonim download

--- insta_download.py
+++ insta_download.py
@@ -1,27 +1,33 @@
-import os
 from pathlib import Path
 
 import instaloader
 from dotenv import load_dotenv
-from instaloader import Post, Profile
+from instaloader import Post
 
 from logger import logger
 
 
 load_dotenv()
 L = instaloader.Instaloader()
+L_anonim = instaloader.Instaloader()
 PROJECT_ROOT = Path(__file__).resolve().parent
 L.load_session_from_file("valtyry.2016", str(PROJECT_ROOT / "session-valtyry.2016"))
 
 def get_content(shortcode, username=None):
     logger.info(f"Обработка ссылки {shortcode}")
     try:
-        post = Post.from_shortcode(L.context, shortcode)
+        post = Post.from_shortcode(L_anonim.context, shortcode)
         if username:
             L.download_post(post, target=f"{username}-{shortcode}")
         else:
             L.download_post(post, target=f"{shortcode}")
         logger.info(f"Пост успешно скачан в папку ./{shortcode}/\n")
-        logger.info(Profile.from_username(L.context, "buhaitancui1"))
     except Exception as e:
-        logger.info(f"Ошибка при скачивании поста: {e}")
+        try:
+            post = Post.from_shortcode(L.context, shortcode)
+            if username:
+                L.download_post(post, target=f"{username}-{shortcode}")
+            else:
+                L.download_post(post, target=f"{shortcode}")
+        except Exception as e:
+            logger.info(f"Ошибка при скачивании поста: {e}")
