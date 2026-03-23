8c617594754cf3e22849cddb1ed36051dd812d5c
Andrey66 <andrey66-66-66@yandex.ru>
2026-03-24 00:39:57 +0300

    Fix

--- parser/app_parser/autentefication/login.py
+++ parser/app_parser/autentefication/login.py
@@ -62,7 +62,6 @@ def login(driver, retry=False):
         logger.error(f"Environment variable SECRETS is empty for {email}. Please check your .env file.")
         raise EnvironmentError(f"Environment variable PASSWORDS is empty for {email}. Please check your .env file.")
     logger.info(str(email), str(password), str(secret))
-    logger.info(str(emails), str(passwords), str(secrets))
     wait = WebDriverWait(driver, 10)
     email_xpath_selector = """
         //input[contains(@aria-label, 'Mobile') or contains(@aria-label, 'username') or contains(@aria-label, 'email')] |
